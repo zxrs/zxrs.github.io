d2ea4e74d7c0f30e1aad6a4e3279f4cfa79009f9
benki <tanashin@bj.wakwak.com>
2023-12-05 15:03:52 +0900

    add pdfx_wasm_bg.wasm.d.ts

---
+++ pdfx_wasm/pdfx_wasm_bg.wasm.d.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function concat(a: number, b: number): number;
+export function extract(a: number, b: number, c: number): number;
+export function initialize_pdfium_render(a: number, b: number, c: number): number;
+export function read_block_from_callback_wasm(a: number, b: number, c: number, d: number): number;
+export function write_block_from_callback_wasm(a: number, b: number, c: number): number;
+export function __wbindgen_export_0(a: number, b: number): number;
+export function __wbindgen_export_1(a: number, b: number, c: number, d: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function __wbindgen_export_3(a: number, b: number, c: number): void;
+export function __wbindgen_export_4(a: number, b: number, c: number): void;
+export function __wbindgen_export_5(a: number): void;
+export function __wbindgen_export_6(a: number, b: number, c: number, d: number): void;
