398df3531a6914177ef5a7eeacc88372e18dfc06
zxrs <tanashin@bj.wakwak.com>
2026-01-29 17:30:52 +0900

    add 40hz

---
+++ 40hz/index.js
@@ -0,0 +1,10 @@
+const onload = () => {
+    const audio = new AudioContext();
+    const oscillator = audio.createOscillator();
+    oscillator.type = "sine";
+    oscillator.frequency.setValueAtTime(40, audio.currentTime);
+    oscillator.connect(audio.destination);
+    oscillator.start();
+};
+
+window.addEventListener("click", onload);
